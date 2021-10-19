df8c826e6fa1ceb85c1142ff7d13717cda4ce184
Alden Lau <aldenlau2002@gmail.com>
2021-10-19 03:03:05 -0700

    Add functionality to AddTask component, create addNewTask function in App.js, map tasks to DayTasks components

--- src/AddTask.js
+++ src/AddTask.js
@@ -3,6 +3,10 @@ import React, {useState} from 'react';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
 
+
+function getFormattedDate(date) {
+    return '' + date.getFullYear()+'/'+(date.getMonth() > 9 ? ''+date.getMonth(): '0'+date.getMonth())+'/'+(date.getDate() > 9 ? ''+date.getDate(): '0'+date.getDate())
+}
 function AddTask({taskHandler, roundTime}) {
     const [startDate, setStartDate] = useState(new Date());
     const [dueDate, setDueDate] = useState(new Date());
@@ -15,19 +19,20 @@ function AddTask({taskHandler, roundTime}) {
             <p>Add new task</p>
             <input onChange={e => setName(e.target.value)}/>
             <input onChange={e => setDescription(e.target.value)}/>
-            <input type='number' onChange={e => setTotalTime(e.target.value)}/>
-            <input type='number' onChange={e=> setPrio(e.target.value)}/>
+            <input type='number' onChange={e => setTotalTime(parseInt(e.target.value))}/>
+            <input type='number' onChange={e=> setPrio(parseInt(e.target.value))}/>
             <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
             <DatePicker selected={dueDate} onChange={(date)=>setDueDate(date)} />
             <button 
                 type='button' 
                 onClick={() => taskHandler(
-                    {time:Math.round(totalTime/roundTime)*roundTime, 
+                    {time:Math.ceil(totalTime/roundTime)*roundTime, 
                     title:name, 
                     desc:description, 
                     priority: prio,
-                    startDate: startDate,
-                    dueDate: dueDate
+                    startDate: getFormattedDate(startDate),
+                    dueDate: getFormattedDate(dueDate),
+                    onClick: () => alert('b')
                     }
                 )}
             >
--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
+
 import logo from './logo.svg';
 import './App.css';
 import DayTasks from './DayTasks.js';
 import AddTask from './AddTask.js';
+import React, {useState} from 'react';
+
 
 function App() {
+  const [tasks, setTasks] = useState(new Map());
+  const [minTime, setMinTime] = useState(30);
+  function addNewTask(newTask) {
+    /**
+     * Given a task, add the task to the tasks state.
+     */
+    let copy = new Map();
+
+    if (tasks.has(newTask.dueDate)) {
+      for (let [date,dateTasks] of tasks.entries()) {
+        copy.set(date, dateTasks);
+        if (date==newTask.dueDate) {
+          copy.get(date).push(newTask);
+        }
+      }
+    }
+    else {
+      let found = false;
+      for (let [date, dateTasks] of tasks.entries()) {
+        if (!found && date>newTask.dueDate) {
+          copy.set(newTask.dueDate, [newTask]);
+          found = true;
+        }
+        copy.set(date, dateTasks);
+      }
+      if (!found) {
+        copy.set(newTask.dueDate, [newTask]);
+      }
+    }
+
+    setTasks(copy);
+    //console.log(newTask.dueDate);
+    //console.log(tasks['2021/09/19'][0].title);
+    for (let [date, dateTasks] of tasks) {
+      console.log(date);
+      for (let dateTask of dateTasks) {
+        console.log(dateTask.title);
+      }
+    }
+  }
+  let taskDates = [];
+  tasks.forEach((dateTasks, date) => taskDates.push(<DayTasks date={date} tasks={dateTasks} />))
 
   return (
     <div className="App">
-      <DayTasks date={Date()} tasks={[{time:5, title:'test', desc:'a test object', priority:0, onClick:()=>alert('bruh')}]}/>
-      <AddTask />
+      {taskDates}
+      <AddTask taskHandler={task => addNewTask(task)} roundTime={minTime}/>
     </div>
   );
 }
